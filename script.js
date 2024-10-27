69ffb7fd3727e9503f74bbe19ff7a3076e4a6cfa
hampboard <cyrussandrei19@gmail.com>
2024-10-27 21:27:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('guest').onclick = function() {
+    alert("You selected Guest Account!");
+};
+
+document.getElementById('email').onclick = function() {
+    alert("You selected Email Account!");
+};
+
+document.getElementById('icloud').onclick = function() {
+    alert("You selected iCloud Account!");
+};
